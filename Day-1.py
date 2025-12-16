77029c52242c152d3b050d1d03b9a426376e6203
Nanda Kishore Reddy Nallavadla <nallavadlanandakishorereddy@gmail.com>
2025-12-16 19:05:06 +0000

    intial commit

---
+++ Day-1.py
@@ -0,0 +1,9 @@
+#inputs to take float numbers
+speed = float(input("Enter speed: "))
+time = float(input("Enter time: "))
+ 
+# Calculate distance
+distance = speed * time
+ 
+# Print the result
+print("Distance traveled:", distance)
